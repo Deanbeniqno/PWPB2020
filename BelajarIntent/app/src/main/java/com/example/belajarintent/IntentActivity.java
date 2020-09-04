79105a31fe460c96c809094b221966ab34244bef
Dean Beniqno A <dbpower20@gmail.com>
2020-09-04 16:59:51 +0700

    Intent dan Constraint

---
+++ BelajarIntent/app/src/main/java/com/example/belajarintent/IntentActivity.java
@@ -0,0 +1,17 @@
+package com.example.belajarintent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+
+}
